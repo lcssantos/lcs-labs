da2df56e19764b76c79fd0fa1b531addf6c553e8
Lucas dos Santos <lucasds@outlook.com>
2017-04-13 10:30:26 -0300

    create project

---
+++ LCS.Core.Domain/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCS.Core.Domain
+{
+    public class BaseModel
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreateAt { get; set; }
+    }
+}
---
+++ LCS.Test.UnitTests/TestingBaseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LCS.Core.Domain;
+
+namespace LCS.Test.UnitTests
+{
+    [TestClass]
+    public class TestingBaseModel
+    {
+        [TestMethod]
+        public void TestId()
+        {
+            var id = Guid.NewGuid();
+            var model = new BaseModel();
+
+            model.Id = id;
+
+            Assert.AreEqual(id, model.Id);
+        }
+    }
+}
